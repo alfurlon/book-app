f38145be738e4c55d497fc35ff14c81bd1aac84d
alfurlon <furlong.austin@gmail.com>
2023-11-27 21:02:43 -0500

    start work on login

--- book-ui/book-ui/app/login/page.tsx
+++ book-ui/book-ui/app/login/page.tsx
@@ -3,6 +3,14 @@ export default function Home() {
     return (
       <main className="h-screen bg-none">
         <h1>login/signup form</h1>
+        <form action="">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" />
+          <label htmlFor="password">Password</label>
+          <input type="text" name="password" />
+          <label htmlFor="passwordConfirm">Password Confirm</label>
+          <input type="text" name="passwordConfirm" />
+        </form>
       </main>
     )
   }
