36862131a83cdf3257cd41c558fbf9d10cb89f2c
alfurlon <furlong.austin@gmail.com>
2023-12-04 21:30:13 -0500

    comment

--- book-api/controllers/bookController.js
+++ book-api/controllers/bookController.js
@@ -4,6 +4,8 @@ const User = require('./../models/userModel');
 const AppError = require('./../util/appError');
 const catchAsync = require('./../util/catchAsync');
 
+// !! delete this comment
+
 exports.getAllBooks = catchAsync(async (req, res, next) => {
     const books = await Book.find({});
     const authors = await Author.find({});
