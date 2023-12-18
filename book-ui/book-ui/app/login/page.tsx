6cef9a779d1ca438114dbdd286972d22769598f0
alfurlon <furlong.austin@gmail.com>
2023-12-17 21:53:52 -0500

    remove comments

--- book-ui/book-ui/app/login/page.tsx
+++ book-ui/book-ui/app/login/page.tsx
@@ -69,7 +69,6 @@ export default function Home() {
           }
         })
           .then(response => {
-            // console.log(response)
             login(response.data.token)
           })
           .catch(error => {
